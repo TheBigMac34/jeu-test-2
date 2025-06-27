3ae73e22a435643a2fbd79d16fe2b4c681cfd039
TheBigMac34 <marouanetouil0@gmail.com>
2025-06-27 12:42:41 +0200

    Add files via upload

--- jeux-test-2/Global.gd
+++ jeux-test-2/Global.gd
@@ -1,1 +1,34 @@
 extends Node
+
+var vies_max = 5
+var vies_actuelles = vies_max
+
+signal heal
+
+func perdre_vie():
+	vies_actuelles -= 1
+	vies_actuelles = clamp(vies_actuelles, 0, vies_max)
+	print("Vies restantes : ", vies_actuelles)
+	var ui = get_tree().current_scene.get_node_or_null("UI")
+	if ui:
+		ui.update_vie()
+	if vies_actuelles <= 0:
+		game_over()
+
+
+func gagner_vie():
+	vies_actuelles += 1
+	vies_actuelles = clamp(vies_actuelles, 0, vies_max)
+	print("Vies actuelles après soin : ", vies_actuelles)
+	var ui = get_tree().current_scene.get_node_or_null("UI")
+	if ui:
+		ui.update_vie()
+	emit_signal("heal")
+
+
+
+
+
+func game_over():
+	print("GAME OVER !")
+	#get_tree().change_scene_to_file("res://game_over.tscn")  # ou autre scène
